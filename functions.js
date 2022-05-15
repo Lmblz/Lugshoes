d7f281038dafa69f229c30bb425e4ff696ecbb84
lambo <lambolez.th@gmail.com>
2022-05-15 10:47:58 +0200

    colors clicked + sizes clicked img replacement (urls not working err404)

---
+++ functions.js
@@ -0,0 +1,21 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'https://lugus-hiring.frb.io/product', true);
+
+
+
+window.onload = function() {
+    request.onload = function() {
+        var donnees = JSON.parse(request.responseText);
+        console.log(donnees);
+
+        var title = document.querySelector('.title > h2');
+        title.textContent = donnees.product.title;
+
+        var desc = document.querySelector('.desc > p');
+        desc.textContent = donnees.product.description;
+
+    }
+    request.send();
+};
+
+
