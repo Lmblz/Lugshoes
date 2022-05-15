d4a4291e23fa840b875ea4dc574571a66129ad5d
lambo <lambolez.th@gmail.com>
2022-05-15 22:53:05 +0200

    final commit

--- functions.js
+++ functions.js
@@ -3,14 +3,14 @@ request.open('GET', 'https://lugus-hiring.frb.io/product', true);
 
 window.onload = function() {
     request.onload = function() {
-        var donnees = JSON.parse(request.responseText);
-        console.log(donnees);
+        var data = JSON.parse(request.responseText);
+        console.log(data);
 
         var title = document.querySelector('.title > h2');
-        title.textContent = donnees.product.title;
+        title.textContent = data.product.title;
 
         var desc = document.querySelector('.desc > p');
-        desc.textContent = donnees.product.description;
+        desc.textContent = data.product.description;
 
     }
     request.send();
